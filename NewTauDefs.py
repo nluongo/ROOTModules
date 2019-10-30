bd2cf94d3974c6a312d450e20346dc51c5c85888
Nicholas Luongo <nicholas.andrew.luongo@cern.ch>
2019-10-30 13:40:16 +0100

    Add central tower seed functions, tob_to_event, and update truth-matching logic

--- NewTauDefs.py
+++ NewTauDefs.py
@@ -1,3 +1,6 @@
+from ROOTDefs import load_root_layer_to_class
+from ROOTClassDefs import Event
+
 # Format cell Et arrays for 3x3 layers in such a way as to maintain [eta][phi] setup with my existing code
 def po_3x3_cells_to_array(formatted_array, po_vector):
     formatted_array[0] = po_vector[0]
@@ -127,41 +130,134 @@ def getSeedCell(cellEt):
       maxSeedEt = cellEt[eta][phi]
   return maxSeedEta, phi 
 
+def getTowerEt(event, eta, phi):
+    '''
+    Get the Et of a tower in a TOB by summing up all cells in that tower.
+
+    :param tob: TOB object
+    :param eta: Eta index of the tower in the context of a 3x3 layer i.e. 0-2
+    :param phi: Phi index of the tower in the context of a 3x3 layer i.e. 0-2
+    :return: Et value for the tower
+    '''
+    towerEt = event.l0_layer.cell_et[eta][phi] + event.l3_layer.cell_et[eta][phi] + event.had_layer.cell_et[eta][phi]
+    for i in range(4):
+        supercell_eta = i + eta * 4
+        towerEt += event.l1_layer.cell_et[supercell_eta][phi]
+        towerEt += event.l2_layer.cell_et[supercell_eta][phi]
+
+    return towerEt
+
+# Determine if central tower of TOB is local max
+def isCentralTowerSeed(event):
+    '''
+    Determine if the central tower of the given event has a max in the central tower 
+    '''
+    # Get Et of central tower
+    centralTowerEt = getTowerEt(event, 1, 1)
+    # Check towers in bottom left corner which must be greater than or equal per central tower seeding
+    if centralTowerEt < getTowerEt(event, 0, 1) or centralTowerEt < getTowerEt(event, 0, 0) or centralTowerEt < getTowerEt(event, 1, 0):
+        return 0
+    # Check all other towers which must be strictly greater than per central tower seeding
+    elif centralTowerEt <= getTowerEt(event, 0, 2) or centralTowerEt <= getTowerEt(event, 1, 2) or centralTowerEt <= getTowerEt(event, 2, 0) or centralTowerEt <= getTowerEt(event, 2, 1) or centralTowerEt <= getTowerEt(event, 2, 2):
+        return 0
+    else:
+        return 1
+
 # Truth-match taus to TOBs
 def eventTruthMatchedTOBs(event):
-  '''
-  Return TOBs that have been matched to true taus. TOBs are required to have a seed as defined by eFEX firmware and lie within eta of 1.4. True taus are required to have Pt > 20 GeV.
+    '''
+    Return TOBs that have been matched to true taus. Only TOBs that pass Run-II seeding will be matched
 
-  :param event: Event in ROOT TTree holding truth, reco, and TOB information
+    :param event: Event in ROOT TTree holding truth, reco, and TOB information
 
-  :return: List of truth-matched TOBs, the first element of the list is the TOB and the second is the Pt of the true tau it has been matched to.
-  '''
-  from ROOT import TVector3
+    :return: List of truth-matched TOBs, the first element of the list is the TOB and the second is the Pt of the true tau it has been matched to.
+    '''
+    from ROOT import TVector3
 
-  matchedTOBs = []
+    matchedTOBs = []
 
-  # Loop over true taus in event
-  for i in range(event.truth_SelectedTau_n):
-    # Build truth vector
-    trueVector = TVector3(0, 0, 0)
-    trueVector.SetPtEtaPhi(event.truth_SelectedTau_tlv_pt[i], event.truth_SelectedTau_tlv_eta[i], event.truth_SelectedTau_tlv_phi[i])
+    # Loop over true taus in event
+    for i in range(event.truth_SelectedTau_n):
+        # Build truth vector
+        trueVector = TVector3(0, 0, 0)
+        trueVector.SetPtEtaPhi(event.truth_SelectedTau_tlv_pt[i], event.truth_SelectedTau_tlv_eta[i], event.truth_SelectedTau_tlv_phi[i])
 
-    # Find TOB with minimum dR from reconstructed tau
-    minTrueTOBdR = float('inf')
-    for j, tob in enumerate(event.efex_AllTOBs):
-      tobVector = TVector3(0, 0, 0)
-      tobVector.SetPtEtaPhi(tob.largeTauClus(), tob.eta(), tob.phi())
-      trueTOBdR = trueVector.DeltaR(tobVector) 
-  
-      if trueTOBdR < minTrueTOBdR:
-        minTrueTOBdR = trueTOBdR
-        closestTOB = tob
-        closestj = j
+        # Find reco tau with minimum dR from truth tau
+        minTrueRecodR = float('inf')
+        trueRecodR = float('inf')
+        closestReco = None
+        closestRecoNum = -1
+        for j in range(event.reco_SelectedTau_n):
+            # Build reco vector
+            recoVector = TVector3(0, 0, 0)
+            recoVector.SetPtEtaPhi(event.reco_SelectedTau_tlv_pt[j], event.reco_SelectedTau_tlv_eta[j], event.reco_SelectedTau_tlv_phi[j])
+            trueRecodR = trueVector.DeltaR(recoVector)
 
-    # If closest TOB is too far from truth then throw away true
-    if minTrueTOBdR > 0.3:
-      continue
+            if trueRecodR < minTrueRecodR:
+                minTrueRecodR = trueRecodR
+                closestReco = recoVector
+                closestRecoNum = j
 
-    matchedTOBs.append([closestTOB, event.truth_SelectedTau_tlv_pt[i], closestj])
+        if minTrueRecodR > 0.3:
+            matchedTOBs.append([-1, event.truth_SelectedTau_tlv_pt[i], event.truth_SelectedTau_tlv_eta[i], -1, -1, i, -1, -1])
+            continue
+
+        # Find TOB with minimum dR from reconstructed tau
+        minRecoTOBdR = float('inf')
+        recoTOBdR = float('inf')
+        closestTOB = None
+        closestTOBNum = -1
+        for k, tob in enumerate(event.efex_AllTOBs):
+            # 3.99 to be consistent with Josefina's code
+            if not tob.ppmIsMaxCore(3.99):
+                continue
+            
+            tobVector = TVector3(0, 0, 0)
+            tobVector.SetPtEtaPhi(tob.largeTauClus(), tob.eta(), tob.phi())
+            recoTOBdR = closestReco.DeltaR(tobVector) 
     
-  return matchedTOBs
+            if recoTOBdR < minRecoTOBdR:
+                minRecoTOBdR = recoTOBdR
+                closestTOB = tob
+                closestTOBNum = k
+
+        # If closest TOB is too far from reco then throw away true
+        if minRecoTOBdR > 0.3:
+            matchedTOBs.append([-1, event.truth_SelectedTau_tlv_pt[i], event.truth_SelectedTau_tlv_eta[i], event.reco_SelectedTau_tlv_pt[closestRecoNum], event.reco_SelectedTau_tlv_eta[closestRecoNum], i, -1, -1])
+            continue
+
+        matchedTOBs.append([closestTOB, event.truth_SelectedTau_tlv_pt[i], event.truth_SelectedTau_tlv_eta[i], event.reco_SelectedTau_tlv_pt[closestRecoNum], event.reco_SelectedTau_tlv_eta[closestRecoNum], i, closestRecoNum, closestTOBNum])
+      
+    return matchedTOBs
+
+def event_from_tob(tree, tob):
+    '''
+    Build a custom Event class instance from a TOB. Intended for formatting Et from TOB into 2D layers
+
+    :param tree: Custom Tree class instance holding TOBs
+    :param tob: TOB object
+    :return: Custom Event class instance
+    ''' 
+    cells = createCellLists(tob)
+
+    l0_cells = [0]*9
+    l1_cells = [0]*36
+    l2_cells = [0]*36
+    l3_cells = [0]*9
+    had_cells = [0]*9
+
+    po_3x3_cells_to_array(l0_cells, cells[0])
+    po_12x3_cells_to_array(l1_cells, cells[1])
+    po_12x3_cells_to_array(l2_cells, cells[2])
+    po_3x3_cells_to_array(l3_cells, cells[3])
+    po_3x3_cells_to_array(had_cells, cells[4])
+
+    l0_layer = load_root_layer_to_class(tree, l0_cells, 0)
+    l1_layer = load_root_layer_to_class(tree, l1_cells, 1)
+    l2_layer = load_root_layer_to_class(tree, l2_cells, 2)
+    l3_layer = load_root_layer_to_class(tree, l3_cells, 3)
+    had_layer = load_root_layer_to_class(tree, had_cells, 4)
+
+    tob_event = Event(tree, l0_layer, l1_layer, l2_layer, l3_layer, had_layer, 0, 1, 0)
+
+    return tob_event
